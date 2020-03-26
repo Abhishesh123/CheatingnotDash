acc73badc7c6c7131267b01d88c99983ea84c06c
jiyo-india <mishra.abhi9619@gmail.com>
2020-03-26 18:15:14 +0530

    26march2020

--- dashboard/cheatingdash/views.py
+++ dashboard/cheatingdash/views.py
@@ -3,11 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from cheatingdash.models import PaytmHistory,StatusHistory
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 
 # Create your views here.
+@login_required(login_url='/login/')
 def Index(request):
-    return HttpResponse('Thanks')
+    user = User.objects.all()
+    print(user)
+    return render(request,'homepage.html',{'users':user})
 
 
 def Login(request):
